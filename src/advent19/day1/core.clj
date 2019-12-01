f423b0595eabe5e7c3999e38bfe7557b4799ff36
gulundin <gu.lundin@gmail.com>
2019-12-01 23:16:18 +0100

    day 1b

--- src/advent19/day1/core.clj
+++ src/advent19/day1/core.clj
@@ -2,20 +2,30 @@
   (:require [clojure.string :as str]))
 
 (defn parse [path]
-  (-> path
+  "Into an int seq"
+  (->> path
       slurp
-      str/split-lines))
+      str/split-lines
+      (map (fn [s] (Integer/parseInt s)))))
 
-(defn fuel-for-module [mass]
+(defn fuel-for-weight [mass]
+  "Does not take the weight of the fuel itself into account"
   (-> mass
       (/ 3)
       Math/floor
       int
       (- 2)))
 
+(defn fuel-for-module-of-weight [m]
+  "Accounts for the weight of the fuel itself"
+  (loop [total-fuel 0
+         new-fuel (fuel-for-weight m)]
+    (if (<= new-fuel 0)
+      total-fuel
+      (recur (+ total-fuel new-fuel) (fuel-for-weight new-fuel)))))
+
 (def total-fuel
-  (->> "src/advent19/day1/inputa.txt"
+  (->> "src/advent19/day1/input.txt"
        parse
-       (map (fn [s] (Integer/parseInt s)))
-       (map fuel-for-module)
-       (reduce + 0)))
+       (map fuel-for-module-of-weight)
+       (reduce +)))
