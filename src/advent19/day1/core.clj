68309920a77080d27d09d7d4e87c7b9d398f5b1d
gulundin <gu.lundin@gmail.com>
2019-12-02 01:17:20 +0100

    day1 improvements

--- src/advent19/day1/core.clj
+++ src/advent19/day1/core.clj
@@ -1,5 +1,6 @@
 (ns advent19.day1.core
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.core.async :as a]))
 
 (defn fuel-for-weight [mass]
   "Does not take the weight of the fuel itself into account"
@@ -9,6 +10,7 @@
 
 (def total-fuel-for-weight
   "Accounts for the weight of the fuel itself"
+  ; memoization is thread-safe, but I suspect that thread-local memoization would be faster
   (memoize (fn [mass]
              (if (<= mass 0)
                0
@@ -16,8 +18,20 @@
                  (+ fuel (total-fuel-for-weight fuel)))))))
 
 (defn total-fuel []
-  (with-open [reader (io/reader "src/advent19/day1/input.txt")]
-    (->> (line-seq reader)
-         (map #(Integer/parseInt %))
-         (map total-fuel-for-weight)
-         (reduce +))))
+  (let [x-form (comp
+                 (map #(Integer/parseInt %))
+                 (map total-fuel-for-weight))]
+    (with-open [reader (io/reader "src/advent19/day1/input.txt")]
+      (transduce x-form + (line-seq reader)))))
+
+(defn total-fuel-parallel []
+  (let [x-form (comp
+                 (map #(Integer/parseInt %))
+                 (map total-fuel-for-weight))]
+    (with-open [reader (io/reader "src/advent19/day1/input.txt")]
+      (let [n-processors (.availableProcessors (Runtime/getRuntime))
+            lines (line-seq reader)
+            in (a/to-chan lines)
+            out (a/chan)]
+        (a/pipeline n-processors out x-form in)
+        (a/<!! (a/reduce + 0 out))))))
