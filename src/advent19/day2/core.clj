262dc1f1ed79bf475da669dd1684d812c2722fba
gulundin <gu.lundin@gmail.com>
2019-12-02 21:36:16 +0100

    cleaned up day 2

--- src/advent19/day2/core.clj
+++ src/advent19/day2/core.clj
@@ -2,60 +2,42 @@
   (:require [clojure.string :as str]
             [clojure.core.logic :as l]))
 
-(defn parse [path]
-  (vec (as-> path v
-             (slurp v)
-             (str/split v #",")
-             (map read-string v))))
-
-(defn create-execution [program]
-  {:pc 0 :program program})
-
-(defn parse-op [opcode]
-  (cond
-    (= 1 opcode) {:op +, :args 3}
-    (= 2 opcode) {:op *, :args 3}
-    (= 99 opcode) {:args 0, :stopped true}))
-
-(defn apply-op [op args program]
-  (let [in-addresses (subvec args 0 (dec (count args)))
-        in (map (fn [n] (nth program n)) in-addresses)
-        res (apply op in)
-        res-address (last args)]
-    (assoc program res-address res)))
-
-(defn execute-step [{:keys [pc program]}]
-  (let [opcode (nth program pc)
-        {op :op, args :args, stopped :stopped} (parse-op opcode)
-        next-pc (+ pc args 1)
-        args (subvec program (inc pc) next-pc)]
-    (if stopped
-      {:program program
-       :stopped true}
-      {:program (apply-op op args program)
-       :pc      next-pc})))
-
-(defn execute-until-stopped [initial-state]
-  (loop [state initial-state]
-    (if (contains? state :stopped)
-      (:program state)
-      (recur (execute-step state)))))
-
 (defn set-input [noun verb program]
   (-> program
       (assoc 1 noun)
       (assoc 2 verb)))
 
-(defn get-result [program]
-  (first program))
+(defn create-execution [program]
+  {:pc 0 :program program})
 
+(defn execute-step [{:keys [pc program]}]
+  (let [op (get program pc)
+        arg1 (get program (get program (+ pc 1)))
+        arg2 (get program (get program (+ pc 2)))
+        res-address (get program (+ pc 3))]
+    (case op
+      1 {:pc (+ pc 4), :program (assoc program res-address (+ arg1 arg2))}
+      2 {:pc (+ pc 4), :program (assoc program res-address (* arg1 arg2))}
+      99 {:stopped true, :program program})))
+
+(defn execute-until-stopped [initial-state]
+  (loop [state initial-state]
+    (if (contains? state :stopped)
+      (get state :program)
+      (recur (execute-step state)))))
 
 (defn execute [program noun verb]
   (->> program
        (set-input noun verb)
        create-execution
        execute-until-stopped
-       get-result))
+       first))
+
+(defn parse [path]
+  (vec (as-> path v
+             (slurp v)
+             (str/split v #",")
+             (map read-string v))))
 
 (def the-program
   (parse "src/advent19/day2/input.txt"))
