2674a43ca6b006934d8e9aa886125725fb37326f
gulundin <gu.lundin@gmail.com>
2019-12-07 12:07:54 +0100

    day 7

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/core.async "0.5.527"]
-                 [org.clojure/core.logic "0.8.11"]]
+                 [org.clojure/core.logic "0.8.11"]
+                 [org.clojure/math.combinatorics "0.1.6"]]
   :repl-options {:init-ns advent19.core})
---
+++ src/advent19/day7/core.clj
@@ -0,0 +1,119 @@
+(ns advent19.day7.core
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
+
+; 43210
+;(def input "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
+; 54321
+;(def input "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0")
+; 65210
+;(def input "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,\n1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0")
+(def input (slurp "src/advent19/day7/input.txt"))
+
+(defn str->int-code [text]
+  (->> (str/split text #",")
+       (map read-string)
+       (vec)))
+
+(def program
+  (str->int-code input))
+
+(def ^:dynamic *pipe* nil)
+
+(defn read-addr [computer address]
+  (get-in computer [:program address]))
+
+(defn read-rel [computer offset]
+  (read-addr computer (+ (get computer :pc) offset)))
+
+(defn read-rel-pointer [computer offset]
+  (read-addr computer (read-rel computer offset)))
+
+(defn read-with-mode [computer offset mode]
+  (case mode
+    0 (read-rel-pointer computer offset)
+    1 (read-rel computer offset)))
+
+(defn write-addr [computer address value]
+  (assoc-in computer [:program address] value))
+
+(defn write-rel-pointer [computer offset value]
+  (write-addr computer (read-rel computer offset) value))
+
+(defn inc-pc [computer increment]
+  (update computer :pc (partial + increment)))
+
+(defn mode [modes n]
+  (mod (nth (iterate #(quot % 10) modes) n) 10))
+
+(defn binary-op [op computer modes]
+  (let [res (op (read-with-mode computer 1 (mode modes 0))
+                (read-with-mode computer 2 (mode modes 1)))]
+    (inc-pc (write-rel-pointer computer 3 res) 4)))
+
+(defn output [computer modes]
+  (let [val (read-with-mode computer 1 (mode modes 0))]
+    (inc-pc (assoc computer :output val) 2)))
+
+(defn input [computer]
+  (let [input-data (get computer :phase (get computer :input))
+        new-computer (dissoc computer :phase)]
+    (inc-pc (write-rel-pointer new-computer 1 input-data) 2)))
+
+(defn jump [computer condition modes]
+  (let [subject (read-with-mode computer 1 (mode modes 0))
+        destination (read-with-mode computer 2 (mode modes 1))]
+    (if (condition subject)
+      (assoc computer :pc destination)
+      (inc-pc computer 3))))
+
+(defn stop [computer]
+  (assoc computer :stopped true))
+
+(defn op-code [instruction]
+  (mod instruction 100))
+
+(defn modes [instruction]
+  (quot instruction 100))
+
+(defn bool->int [b]
+  (if b 1 0))
+
+(defn execute-computer [initial-computer]
+  (loop [computer initial-computer]
+    (let [instruction (read-rel computer 0)
+          modes (modes instruction)
+          next-computer (case (op-code instruction)
+                          1 (binary-op + computer modes)
+                          2 (binary-op * computer modes)
+                          3 (input computer)
+                          4 (output computer modes)
+                          5 (jump computer (comp not zero?) modes)
+                          6 (jump computer zero? modes)
+                          7 (binary-op (comp bool->int <) computer modes)
+                          8 (binary-op (comp bool->int =) computer modes)
+                          (stop computer))]
+      (if (:stopped next-computer)
+        next-computer
+        (recur next-computer)))))
+
+(defn execute-amplifiers [phase-settings]
+  (loop [phases phase-settings
+         prev-output 0]
+    (if-let [phase (peek phases)]
+      (let [computer {:pc 0 :program program :input prev-output :phase phase}
+            output (:output (execute-computer computer))]
+        (recur (pop phases) output))
+      prev-output)))
+
+(defn answer []
+  (->> (range 5)
+       (combo/permutations)
+       (map execute-amplifiers)
+       (reduce max)))
+
+(defn answer2 []
+  (->> (range 5 10)
+       (combo/permutations)
+       (map execute-amplifiers)
+       (reduce max)))
