6d3ef0f51ecf0d5d15dce9dbae4a6c213e3a6e46
gulundin <gu.lundin@gmail.com>
2019-12-07 13:33:19 +0100

    day7

--- src/advent19/day7/core.clj
+++ src/advent19/day7/core.clj
@@ -2,12 +2,10 @@
   (:require [clojure.string :as str]
             [clojure.math.combinatorics :as combo]))
 
-; 43210
-;(def input "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
-; 54321
-;(def input "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0")
-; 65210
-;(def input "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,\n1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0")
+; 139629729
+;(def input "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5")
+; 18216
+;(def input "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10")
 (def input (slurp "src/advent19/day7/input.txt"))
 
 (defn str->int-code [text]
@@ -17,8 +15,6 @@
 
 (def program
   (str->int-code input))
-
-(def ^:dynamic *pipe* nil)
 
 (defn read-addr [computer address]
   (get-in computer [:program address]))
@@ -53,7 +49,10 @@
 
 (defn output [computer modes]
   (let [val (read-with-mode computer 1 (mode modes 0))]
-    (inc-pc (assoc computer :output val) 2)))
+    (-> computer
+        (assoc :output val)
+        (assoc :yielding true)
+        (inc-pc 2))))
 
 (defn input [computer]
   (let [input-data (get computer :phase (get computer :input))
@@ -79,8 +78,8 @@
 (defn bool->int [b]
   (if b 1 0))
 
-(defn execute-computer [initial-computer]
-  (loop [computer initial-computer]
+(defn schedule [input-computer]
+  (loop [computer input-computer]
     (let [instruction (read-rel computer 0)
           modes (modes instruction)
           next-computer (case (op-code instruction)
@@ -93,27 +92,38 @@
                           7 (binary-op (comp bool->int <) computer modes)
                           8 (binary-op (comp bool->int =) computer modes)
                           (stop computer))]
-      (if (:stopped next-computer)
-        next-computer
+      (if (or (:stopped next-computer)
+              (:yielding next-computer))
+        (dissoc next-computer :yielding)
         (recur next-computer)))))
 
+(defn all-stopped [computers]
+  (->> computers
+       (map :stopped)
+       (remove true?)
+       (empty?)))
+
+(defn pipe-io [computers]
+  (vec (let [ncomputers (count computers)]
+         (for [i (range ncomputers)]
+           (assoc (get computers i) :input (:output (get computers (mod (dec i) ncomputers))))))))
+
 (defn execute-amplifiers [phase-settings]
-  (loop [phases phase-settings
-         prev-output 0]
-    (if-let [phase (peek phases)]
-      (let [computer {:pc 0 :program program :input prev-output :phase phase}
-            output (:output (execute-computer computer))]
-        (recur (pop phases) output))
-      prev-output)))
+  (let [ncomputers (count phase-settings)
+        initial-computers (->> phase-settings
+                               (map (fn [phase] {:pc 0 :program program :input 0 :phase phase}))
+                               (vec))]
+    (loop [computers initial-computers
+           execute-next 0]
+      (if (all-stopped computers)
+        (:output (last computers))
+        (recur (pipe-io (update-in computers [execute-next] schedule))
+               (mod (inc execute-next) ncomputers))))))
 
-(defn answer []
-  (->> (range 5)
-       (combo/permutations)
-       (map execute-amplifiers)
-       (reduce max)))
-
-(defn answer2 []
+(defn max-thrust []
   (->> (range 5 10)
        (combo/permutations)
        (map execute-amplifiers)
        (reduce max)))
+
+(time (max-thrust))
