55d54f39305578c943ada879ebcd8ec00d7a17c1
gulundin <gu.lundin@gmail.com>
2019-12-08 18:14:50 +0100

    day 8

---
+++ src/advent19/day8/core.clj
@@ -0,0 +1,25 @@
+(ns advent19.day8.core
+  (:require [clojure.string :as str]))
+
+(def input (slurp "src/advent19/day8/input.txt"))
+(def width 25)
+(def height 6)
+(def size (* width height))
+
+(defn n-chars [char coll]
+  (count (filter (partial = char) coll)))
+
+(defn answer1 []
+  (->> input
+       (partition size)
+       (apply min-key (partial n-chars \0))
+       (#(* (n-chars \1 %) (n-chars \2 %)))))
+
+(defn answer2 []
+  (->> input
+       (partition size)
+       (apply map vector)
+       (map #(first (filter (partial not= \2) %)))
+       (map {\0 \  \1 \#})
+       (partition width)
+       (map str/join)))
